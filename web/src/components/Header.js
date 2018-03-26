d807bdbae3f215987421954695bbbfd710a43060
nick554 <xiaokong.chen@gmail.com>
2018-03-26 14:22:19 -0400

    add logout bottom in header

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from '../asserts/images/logo.svg';
+import { Icon } from 'antd';
+
 
 export class Header extends React.Component {
     render() {
@@ -9,6 +11,12 @@ export class Header extends React.Component {
                 <div className="App-title">
                     <p className = "project-name">Around</p>
                     <p className = "project-tool">Powered by React</p>
+                    {
+                        this.props.isLoggedIn ?
+                            <a className="logout" onClick={this.props.handleLogout}>
+                                <Icon type="logout"/>&nbsp&nbsp logout
+                            </a> : null
+                    }
                 </div>
             </header>
         );
