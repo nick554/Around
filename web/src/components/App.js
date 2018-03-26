82eccb1c37dfc34daa0ad2880b6784a438a70b44
nick554 <xiaokong.chen@gmail.com>
2018-03-26 15:52:19 -0400

    commit all

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import '../styles/App.css';
 import { Header } from './Header'
 import { Main } from './Main'
+import { Token_KEY } from '../constants'
+
+import '../styles/App.css';
+
 
 class App extends React.Component {
+    state = {
+        isLoggedIn : !! localStorage.getItem(Token_KEY),
+    }
+    handleSuccessLogin = (response) => {
+        localStorage.setItem(Token_KEY, response);
+        this.setState({ isLoggedIn:true })
+    }
+    handleLogout = () => {
+        localStorage.removeItem(Token_KEY);
+        this.setState({ isLoggedIn: false });
+    }
+q
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main />
+        <Header isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout}/>
+        <Main isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout}/>
       </div>
     );
   }
