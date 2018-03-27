b3a352d225b87499e9b88c8882b386f81819564a
nick554 <xiaokong.chen@gmail.com>
2018-03-27 14:54:18 -0400

    done with redirect and url consistency

--- web/src/components/Main.js
+++ web/src/components/Main.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import {Register} from './Register'
+import { Switch, Route, Redirect } from 'react-router-dom';
+import {Home} from "./Home"
 import {Login} from './Login'
+import {Register} from './Register'
+
 
 export class Main extends React.Component {
+    getRedirect = () => {
+        return this.isLoggedIn ? <Redirect to="/home"/> : <Redirect to="/login"/>;
+    };
+    getHome = () => {
+        return this.isLoggedIn ? <Home /> : <Redirect to="/login"/>;
+    };
+    getLogin = () => {
+        return this.isLoggedIn ? <Redirect to="/home"/> : <Login />;
+    };
+    getRegister = () => {
+        return this.isLoggedIn ? <Redirect to="/home"/> : <Register />;
+    };
     render() {
         return (
             <div className = "main">
-                <Register/>
-                <Login/>
+                <Switch>
+                    <Route exact path="/" render = {this.getRedirect} />
+                    <Route path="/home" render = {this.getHome} />
+                    <Route path="/login" render = {this.getLogin} />
+                    <Route path="/register" render = {this.getRegister} />
+                    <Route render = {this.getRedirect} />
+                </Switch>
             </div>
         );
     }
