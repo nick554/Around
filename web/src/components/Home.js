331704db239bcc273c5973de1af7c75fc4c5c5f0
nick554 <xiaokong.chen@gmail.com>
2018-03-29 22:59:00 -0400

    add frame to utilize geolocation

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import { Tabs, Button } from 'antd';
-import {TabPane} from '../constants'
+import { TabPane, GEO_OPTIONS } from '../constants'
 
 
 export class Home extends React.Component {
+    componentDidMount() {
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
+        if ( "geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessGetGeolocation,
+                this.onFailureGetGeolocation,
+                GEO_OPTIONS
+            );
+        } else {
+
+        }
+    }
+
+    onSuccessGetGeolocation = (position) => {
+        console.log(position);
+    }
+    onFailureGetGeolocation = () => {
+        console.log('false')
+    }
+
+
     render() {
-
-
         return (
             <Tabs tabBarExtraContent={ <Button type = 'primary'>Create New Post</Button> } className="home-tags">
                 <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
--- web/src/constants.js
+++ web/src/constants.js
@@ -2,6 +2,11 @@ import {Form, Tabs} from "antd/lib/index"
 
 export const ServerRoot = 'https://around-75015.appspot.com';
 export const Token_KEY = 'TOKEN_KEY';
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge        : 30000,
+    timeout           : 27000
+};
 
 // components from antd
 export const TabPane = Tabs.TabPane;
