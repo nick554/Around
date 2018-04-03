726b17347d89807dcc84cbce1d7feab73063f069
nick554 <xiaokong.chen@gmail.com>
2018-04-02 20:12:26 -0400

    add animation when the website is trying to achieve the geo-location

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Tabs, Button } from 'antd';
+import {Tabs, Button, Spin} from 'antd';
 import { TabPane, GEO_OPTIONS } from '../constants'
 
 
 export class Home extends React.Component {
     state = {
-        error:'',
+        error: '',
+        loadingGeoLocation: false,
     }
+
     componentDidMount() {
+        this.setState({ loadingGeoLocation: true, error: ''  });
         this.getGeoLocation();
     }
 
@@ -20,21 +23,24 @@ export class Home extends React.Component {
             );
         } else {
             // Barely happen now. Haven't been tested.
-            this.setState({ error:'Your browser does not support geolocation!' });
+            this.setState({ loadingGeoLocation : false, error:'Your browser does not support geolocation!' });
         }
     }
 
     onSuccessGetGeolocation = (position) => {
         const {latitude, longitude } = position.coords;
         console.log(latitude+' '+longitude);
+        this.setState({ loadingGeoLocation: false, error: '' });
     }
     onFailureGetGeolocation = () => {
-        this.setState({ error: 'Failed to load geo location!' });
+        this.setState({ loadingGeoLocation: false, error: 'Failed to load geo location!' });
     }
 
     getPostContent = () => {
         if (this.state.error) {
             return <div>{this.state.error}</div>;
+        } else if ( this.state.loadingGeoLocation ) {
+            return <Spin tip="Loading geo location..."/>;
         } else {
             return <div>have this function</div>;
         }
@@ -43,7 +49,9 @@ export class Home extends React.Component {
     render() {
         return (
             <Tabs tabBarExtraContent={ <Button type = 'primary'>Create New Post</Button> } className="home-tags">
-                <TabPane tab="Posts" key="1">{this.getPostContent()}</TabPane>
+                <TabPane tab="Posts" key="1">
+                    {this.getPostContent()}
+                </TabPane>
                 <TabPane tab="Map View" key="2">Content of tab 2</TabPane>
             </Tabs>
         );
